40b13dcb7c227ebe1b7713dc3ccf90d97ff7f033
Alan Alexis <alanalexisceb@hotmail.com>
2021-08-05 23:32:25 -0500

    Version 1.2.0, se agrego el modo de tarjeta ademas de descripcion y materia

--- main.js
+++ main.js
@@ -16,6 +16,8 @@ valorSubmit.addEventListener('click', () => {
     }
     console.log(contenidoInput)
 	valorTareaInput.value = ""
+    document.getElementById('input_materia').value = ""
+    document.getElementById('input_descripcion').value = ""
 })
 
 valorDelete.addEventListener('click', () => {
@@ -29,15 +31,23 @@ valorDelete.addEventListener('click', () => {
         alert('Debes ingresar una tarea o sólo poner números positivos')
     } else {
         hijo.className = 'eliminado';
-        hijo.append(', tarea terminada a las:  ' + moment().format('LTS'))
+        hijo.append(', terminada: ' + moment().format('LTS'))
     }
 	document.getElementById('input_eliminar_tareas').value = '';
 })
 
 function addElemento(tarea) {
     let tareas = document.getElementById("lista_tareas");
+    let tareasMateria = document.createElement('h3')
+    let saltoLinea = document.createElement('br');
+    tareasMateria = document.getElementById('input_materia').value
+    let tareasDescripcion = document.createElement('h3')
+    tareasDescripcion = document.getElementById('input_descripcion').value
+    let divTarjeta = document.createElement("div");
     let h2 = document.createElement("h2");
+    divTarjeta.setAttribute('class', 'tarjeta')
     h2.setAttribute('id', contador);
     h2.innerHTML = tarea;
-    tareas.appendChild(h2);
+    divTarjeta.append(h2, tareasMateria, saltoLinea, tareasDescripcion)
+    tareas.before(divTarjeta);
 }
