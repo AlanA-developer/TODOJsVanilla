fe9bc417f6e4c53d334adb7326aec54c3a55c0a7
Alan Alexis <alanalexisceb@hotmail.com>
2021-08-06 16:54:31 -0500

    Mejorando el modelo de tarjeta, mejorando animaciones y agregando separaciones, bug de margen en telefonos solucionado

--- main.js
+++ main.js
@@ -14,7 +14,6 @@ valorSubmit.addEventListener('click', () => {
         contador++
         addElemento(contador + ' - ' + contenidoInput)
     }
-    console.log(contenidoInput)
 	valorTareaInput.value = ""
     document.getElementById('input_materia').value = ""
     document.getElementById('input_descripcion').value = ""
@@ -27,7 +26,7 @@ valorDelete.addEventListener('click', () => {
         alert('Esa tarea no existe')
 		document.getElementById('input_eliminar_tareas').value = '';
     }
-    if (valorAEliminar <= 0) {
+    else if(valorAEliminar <= 0) {
         alert('Debes ingresar una tarea o sólo poner números positivos')
     } else {
         hijo.className = 'eliminado';
@@ -37,17 +36,17 @@ valorDelete.addEventListener('click', () => {
 })
 
 function addElemento(tarea) {
-    let tareas = document.getElementById("lista_tareas");
     let tareasMateria = document.createElement('h3')
-    let saltoLinea = document.createElement('br');
-    tareasMateria = document.getElementById('input_materia').value
     let tareasDescripcion = document.createElement('h3')
+    const tareas = document.getElementById("lista_tareas");
+    const lineaDivision = document.createElement('hr')
+    const divTarjeta = document.createElement("div");
+    const h2 = document.createElement("h2");
+    tareasMateria = document.getElementById('input_materia').value
     tareasDescripcion = document.getElementById('input_descripcion').value
-    let divTarjeta = document.createElement("div");
-    let h2 = document.createElement("h2");
     divTarjeta.setAttribute('class', 'tarjeta')
     h2.setAttribute('id', contador);
     h2.innerHTML = tarea;
-    divTarjeta.append(h2, tareasMateria, saltoLinea, tareasDescripcion)
+    divTarjeta.append(h2, tareasMateria, lineaDivision, tareasDescripcion)
     tareas.before(divTarjeta);
 }
