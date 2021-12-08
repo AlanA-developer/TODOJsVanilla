eee91512a5b0da8576a49318f87a052af879684f
Alan Alexis <alanalexisceb@hotmail.com>
2021-12-07 18:48:09 -0600

    Agregando borde blanco a las tarjetas al activar drakmode

--- exports/darkMode.js
+++ exports/darkMode.js
@@ -1,22 +1,23 @@
 export const toggleDarkMode = () => {
     const checkbox = document.getElementById('checkbox');
     const body = document.querySelector('body');
-    const h1 = document.querySelector('h1');
-    const h3 = document.querySelector('h3');
-
+    
     checkbox.addEventListener('change', () => {
-    if (checkbox.checked) {
+        const tarjetas = document.querySelectorAll('.tarjeta');
+        if (checkbox.checked) {
         body.classList.add('dark');
-        h1.classList.add('dark-font');
-        h3.classList.add('dark-font');
         checkbox.nextElementSibling.style.border = '1px solid white';
         checkbox.nextElementSibling.innerHTML = 'Dark mode: On 🌑';
+        tarjetas.forEach(tarjeta => {
+            tarjeta.classList.add('tarjetaBorderWhite');
+        });
     } else {
         body.classList.remove('dark');
-        h1.classList.remove('dark-font');
-        h3.classList.remove('dark-font');
         checkbox.nextElementSibling.style.border = '1px solid black';
-        checkbox.nextElementSibling.innerHTML = 'Dark mode: Off  ☀';
+        checkbox.nextElementSibling.innerHTML = 'Dark mode: Off ☀';
+        tarjetas.forEach(tarjeta => {
+            tarjeta.classList.remove('tarjetaBorderWhite');
+        });
     }  
     });
 }
