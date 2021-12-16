688618e40e3504f4d0e1d40cd39cf8fc303b43c8
Alan Alexis <alanalexisceb@hotmail.com>
2021-12-16 11:55:43 -0600

    App version 4.0.3 arreglando bug tareas vacias

--- exports/exportSaveEdit.js
+++ exports/exportSaveEdit.js
@@ -1,17 +1,27 @@
 export const saveEdit = () => {
-    setTimeout(() => {
-      let buttonSave = document.getElementById("save");
-      buttonSave.addEventListener("click", () => {
-        let actualElement = document.querySelector(".modal");
-        let title = document.getElementById("changeTitle").value;
-        let subject = document.getElementById("changeSubject").value;
-        let description = document.getElementById("changeDescription").value;
-  
-        actualElement.parentElement.childNodes[1].innerHTML = title;
-        actualElement.parentElement.childNodes[5].innerHTML = subject;
-        actualElement.parentElement.childNodes[9].innerHTML = description;
-  
-        actualElement.remove();
-      });
-    }, 50);
-  }
+  setTimeout(() => {
+    let buttonSave = document.getElementById("save");
+    buttonSave.addEventListener("click", () => {
+      let actualElement = document.querySelector(".modal");
+      let title = document.getElementById("changeTitle").value;
+      let subject = document.getElementById("changeSubject").value;
+      let description = document.getElementById("changeDescription").value;
+
+      if (title == "") {
+        title = actualElement.parentElement.childNodes[1].textContent;
+      }
+      if (subject == "") {
+        subject = actualElement.parentElement.childNodes[5].textContent;
+      }
+      if (description == "") {
+        description = actualElement.parentElement.childNodes[9].textContent;
+      }
+
+      actualElement.parentElement.childNodes[1].textContent = title;
+      actualElement.parentElement.childNodes[5].textContent = subject;
+      actualElement.parentElement.childNodes[9].textContent = description;
+
+      actualElement.remove();
+    });
+  }, 50);
+};
