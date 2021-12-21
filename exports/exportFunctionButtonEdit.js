084657fcc88edf06fb3d4264db497fa1d2373ad1
Alan Alexis <alanalexisceb@hotmail.com>
2021-12-20 21:27:50 -0600

    App version 4.0.5 arreglando bugs de botones

--- exports/exportFunctionButtonEdit.js
+++ exports/exportFunctionButtonEdit.js
@@ -17,14 +17,22 @@ export let buttonEdit = async () => {
       //No se puede crear un modal si ya hay uno
       if (modalCount.length == 0) {
         modal.innerHTML = `
+
             <div>
+
               <h1 class="modalTitle"> ¿Qué quieres cambiar de tu tarea? </h1>
+              
               <input type="text" id="changeTitle" placeholder="Título">
               <input type="text" id="changeSubject" placeholder="Materia">
               <input type="text" id="changeDescription" placeholder="Descripción">
+              
+              <br>
               <button id="close" onclick="${closeModal()}">Cancelar</button>
+              <br>
               <button id="save"  onclick="${saveEdit()}">Guardar</button>
+
             </div>
+
           `;
         tarjetas[i].appendChild(modal);
       }
