9e93adf98765599b2babca2a97169bdfca4bfdb3
Alan Alexis <alanalexisceb@hotmail.com>
2021-12-20 21:24:13 -0600

    App version 4.0.4 arreglando bugs menores

--- exports/darkMode.js
+++ exports/darkMode.js
@@ -3,21 +3,32 @@ export const toggleDarkMode = () => {
   const body = document.querySelector("body");
 
   checkbox.addEventListener("change", () => {
+
     const tarjetas = document.querySelectorAll(".tarjeta");
+    
     if (checkbox.checked) {
-      body.classList.add("dark");
-      tarjetas.forEach((tarjeta) => {
-        tarjetas.classList.add("tarjetaBorderWhite");
-      });
-      checkbox.nextElementSibling.style.border = "1px solid white";
-      checkbox.nextElementSibling.innerHTML = "Dark mode: On 🌑";
-    } else {
-      body.classList.remove("dark");
-      tarjetas.forEach((tarjeta) => {
-        tarjeta.classList.remove("tarjetaBorderWhite");
-      });
-      checkbox.nextElementSibling.style.border = "1px solid black";
-      checkbox.nextElementSibling.innerHTML = "Dark mode: Off ☀";
-    }
+
+          body.classList.add("dark");
+
+          tarjetas.forEach((tarjeta) => {
+            tarjeta.classList.add("tarjetaBorderWhite");
+          });
+
+          checkbox.nextElementSibling.style.border = "1px solid white";
+          checkbox.nextElementSibling.innerHTML = "Dark mode: On 🌑";
+      
+      } else {
+          
+          body.classList.remove("dark");
+
+          tarjetas.forEach((tarjeta) => {
+            tarjeta.classList.remove("tarjetaBorderWhite");
+          });
+
+          checkbox.nextElementSibling.style.border = "1px solid black";
+          checkbox.nextElementSibling.innerHTML = "Dark mode: Off ☀";
+
+      }
   });
+
 };
