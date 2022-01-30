865d464c1661c7ca44b32f375888e820d8edd1cf
Alan Alexis <alanalexisceb@hotmail.com>
2022-01-30 16:36:10 -0600

    Mejorando la velocidad de la deteccion de enter

--- exports/exportDetectEnterToAddTask.js
+++ exports/exportDetectEnterToAddTask.js
@@ -21,13 +21,10 @@ export const detectEnterToAddTask = (event) => {
 const createTask = () => {
   //Localizamos el input del valor de la tarea y la guardamos en una variable
   const valorTareaInput = document.getElementById("input_tareas");
-
-  //Guardamos el valor del input en una variable
-  let contenidoInput = valorTareaInput.value;
-
+  
   //Función para determinar la importancia de la tarea
   functionImportanceTask();
 
   //Si el valor del input no está vacío ejecuta la función `addElemento()` de lo contrario no hace nada
-  contenidoInput === "" ? alert("No puedes agregar una tarea vacia") : addElemento(contenidoInput), buttonEdit(), resetInputs();
+  valorTareaInput.value === "" ? alert("No puedes agregar una tarea vacia") : addElemento(valorTareaInput.value), buttonEdit(), resetInputs();
 };
