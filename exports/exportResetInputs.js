9f2076c86703c90e37fe48aae56d6eb3ed398f82
Alan Alexis <alanalexisceb@hotmail.com>
2022-02-17 21:30:45 -0600

    Formateando el código

--- exports/exportResetInputs.js
+++ exports/exportResetInputs.js
@@ -1,7 +1,12 @@
 //Creamos y exportamos la función para regresar los valores de los inputs a su estado original
 export const resetInputs = () => {
-    //Localizamos todos los inputs y los regresamos a su estado original
-    document.getElementById("input_tareas").value = "";
-    document.getElementById("input_materia").value = "";
-    document.getElementById("input_descripcion").value = "";
-  };
+  const resetingInputs = [
+    document.getElementById("input_tareas"),
+    document.getElementById("input_materia"),
+    document.getElementById("input_descripcion"),
+  ];
+
+  resetingInputs.forEach((input) => {
+    input.classList.value = "";
+  });
+};
